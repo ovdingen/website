1d8970e9d0b6a8f6003977bf29f62857ffd40c81
Marc <mail@marctijssen.nl>
2018-01-08 11:42:42 +0100

    Extra row toegevoegd

--- footer.php
+++ footer.php
@@ -1,31 +1,29 @@
   <footer class="page-footer teal">
     <div class="container">
-      <!-- <div class="row">
+      <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">ovdingen</h5>
-          <p class="grey-text text-lighten-4">Tekst</p>
-
-
+          <ul>
+            <li><a class="white-text" href="https://ovdingen.nl/doneer.php">Doneer aan ovdingen.nl!</a></li>
+          </ul>
         </div>
         <div class="col l3 s12">
           <h5 class="white-text">Projecten</h5>
           <ul>
-            <li><a class="white-text" href="#">Link 1</a></li>
-            <li><a class="white-text" href="#">Link 2</a></li>
-            <li><a class="white-text" href="#">Link 3</a></li>
-            <li><a class="white-text" href="#">Link 4</a></li>
+            <li><a class="white-text" href="https://treinviewer.ovdingen.nl" target="blank">Trein Viewer</a></li>
+            <li><a class="white-text" href="https://ovdingen.nl/telefoon.php" target="blank">Telefoon Service</a></li>
+            <!-- <li><a class="white-text" href="#">Link 3</a></li>
+            <li><a class="white-text" href="#">Link 4</a></li> -->
           </ul>
         </div>
         <div class="col l3 s12">
           <h5 class="white-text">Github</h5>
+          <!-- <p class="grey-text text-lighten-4">Wil je meewerken aan de projecten van ovdingen? Kijk dan op onze github.</p> -->
           <ul>
-            <li><a class="white-text" href="#">Link 1</a></li>
-            <li><a class="white-text" href="#">Link 2</a></li>
-            <li><a class="white-text" href="#">Link 3</a></li>
-            <li><a class="white-text" href="afbeeldingscredits.txt" target="blank">afbeeldingscredits</a></li>
+            <li><a class="white-text" href="afbeeldingscredits.txt" target="blank"><i class="fab fa-github"></i></a></li>
           </ul>
         </div>
-      </div> -->
+      </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
@@ -42,6 +40,7 @@
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
+  <script src="js/custom.js"></script>
 
   </body>
   <!-- 
