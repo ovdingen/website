35b2a0acb69115408eb798d228509b932b983509
Marc <mail@marctijssen.nl>
2018-01-08 08:57:14 +0100

    Transfer naar PHP

---
+++ footer.php
@@ -0,0 +1,50 @@
+  <footer class="page-footer teal">
+    <div class="container">
+      <!-- <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">ovdingen</h5>
+          <p class="grey-text text-lighten-4">Tekst</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Projecten</h5>
+          <ul>
+            <li><a class="white-text" href="#">Link 1</a></li>
+            <li><a class="white-text" href="#">Link 2</a></li>
+            <li><a class="white-text" href="#">Link 3</a></li>
+            <li><a class="white-text" href="#">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Github</h5>
+          <ul>
+            <li><a class="white-text" href="#">Link 1</a></li>
+            <li><a class="white-text" href="#">Link 2</a></li>
+            <li><a class="white-text" href="#">Link 3</a></li>
+            <li><a class="white-text" href="afbeeldingscredits.txt" target="blank">afbeeldingscredits</a></li>
+          </ul>
+        </div>
+      </div> -->
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made with <i class="fas fa-heart red-text"></i> by ovdingen
+      <div class="right">
+        <a class="white-text" href="https://github.com/ovdingen/website/blob/master/LICENSE" target="blank">GPL-3.0</a> | <a class="white-text" href="afbeeldingscredits.txt" target="blank">afbeeldingscredits</a>
+      </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+  <!-- 
+  Nee, bedankt. Ik heb geen fijne dag nodig!!!1
+  -->
+</html>
